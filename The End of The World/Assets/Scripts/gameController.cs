ac1b6654c6200ebd801d65d1275d5eaaded920ec
Xalaphas <raphaelrdgss.006@gmail.com>
2024-06-16 00:10:28 -0300

    Add UI para a pontuação ao coletar itens
    Adicionando UI para exibir ao jogador a sua pontuação ao coletar itens

--- The End of The World/Assets/Scripts/gameController.cs
+++ The End of The World/Assets/Scripts/gameController.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameController : MonoBehaviour
 {
     public int totalScore;
+    public Text scoreText;
 
     public static gameController instance;
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+    }
+
+    public void UpdataScoreText()
+    {
+        scoreText.text = totalScore.ToString();
     }
 
 }
--- The End of The World/Assets/Scripts/itens/collectIten.cs
+++ The End of The World/Assets/Scripts/itens/collectIten.cs
@@ -6,7 +6,9 @@ public class collectIten : MonoBehaviour
 {
     private SpriteRenderer sr;
     private CircleCollider2D circle;
+
     public GameObject collected;
+
     public int Score;
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,12 @@ public class collectIten : MonoBehaviour
      void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Player") {
-            sr.enabled = false;
-            circle.enabled = false;
+            sr.enabled = true;
+            circle.enabled = true;
             collected.SetActive(true);  
             
             gameController.instance.totalScore += Score;
+            gameController.instance.UpdataScoreText();
 
             Destroy(gameObject, 0.26f);
         }
